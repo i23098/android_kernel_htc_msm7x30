c7e1b1ab638e5619171db50b08e68cb961755ddf
Santosh Shilimkar <santosh.shilimkar@ti.com>
2011-06-28 12:42:56 -0700

    ARM: mm: Add strongly ordered descriptor support.
    On certain architectures, there might be a need to mark certain
    addresses with strongly ordered memory attributes to avoid ordering
    issues at the interconnect level.
    
    On OMAP4, the asynchronous bridge buffers can only be drained
    with strongly ordered accesses and hence the need to mark the
    memory strongly ordered.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Woodruff Richard <r-woodruff2@ti.com>
    Tested-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/include/asm/mach/map.h
+++ arch/arm/include/asm/mach/map.h
@@ -33,11 +33,12 @@ struct map_desc {
 #define MT_MEMORY_NONCACHED	11
 #define MT_MEMORY_DTCM		12
 #define MT_MEMORY_ITCM		13
-#define MT_DEVICE_STRONGLY_ORDERED 14
-#define MT_MEMORY_R		15
-#define MT_MEMORY_RW		16
-#define MT_MEMORY_RX		17
-#define MT_MEMORY_DMA_READY	18
+#define MT_MEMORY_SO		14
+#define MT_DEVICE_STRONGLY_ORDERED 15
+#define MT_MEMORY_R		16
+#define MT_MEMORY_RW		17
+#define MT_MEMORY_RX		18
+#define MT_MEMORY_DMA_READY	19
 
 #ifdef CONFIG_MMU
 extern void iotable_init(struct map_desc *, int);
