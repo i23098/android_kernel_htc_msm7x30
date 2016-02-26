29bf442481fd1ef346097904c17ded9db4912191
Denis Pauk <pauk.denis@gmail.com>
2016-02-26 22:34:24 +0200

    clean up headers

--- arch/arm/include/asm/unaligned.h
+++ arch/arm/include/asm/unaligned.h
@@ -1,30 +1,19 @@
-#ifndef __ASM_GENERIC_UNALIGNED_H
-#define __ASM_GENERIC_UNALIGNED_H
+#ifndef _ASM_ARM_UNALIGNED_H
+#define _ASM_ARM_UNALIGNED_H
+
+#include <linux/unaligned/le_byteshift.h>
+#include <linux/unaligned/be_byteshift.h>
+#include <linux/unaligned/generic.h>
 
 /*
- * This is the most generic implementation of unaligned accesses
- * and should work almost anywhere.
- *
- * If an architecture can handle unaligned accesses in hardware,
- * it may want to use the linux/unaligned/access_ok.h implementation
- * instead.
+ * Select endianness
  */
-#include <asm/byteorder.h>
-
-#if defined(__LITTLE_ENDIAN)
-# include <linux/unaligned/le_struct.h>
-# include <linux/unaligned/be_byteshift.h>
-# include <linux/unaligned/generic.h>
-# define get_unaligned	__get_unaligned_le
-# define put_unaligned	__put_unaligned_le
-#elif defined(__BIG_ENDIAN)
-# include <linux/unaligned/be_struct.h>
-# include <linux/unaligned/le_byteshift.h>
-# include <linux/unaligned/generic.h>
-# define get_unaligned	__get_unaligned_be
-# define put_unaligned	__put_unaligned_be
+#ifndef __ARMEB__
+#define get_unaligned	__get_unaligned_le
+#define put_unaligned	__put_unaligned_le
 #else
-# error need to define endianess
+#define get_unaligned	__get_unaligned_be
+#define put_unaligned	__put_unaligned_be
 #endif
 
-#endif /* __ASM_GENERIC_UNALIGNED_H */
+#endif /* _ASM_ARM_UNALIGNED_H */
