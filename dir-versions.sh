1dd287a0351ad5053d2163309d594b9793ed7f01
Denis Pauk <pauk.denis@gmail.com>
2016-09-03 22:08:47 +0300

    add notice about msm_reboot

--- dir-versions.sh
+++ dir-versions.sh
@@ -4,10 +4,6 @@ git checkout HEAD -- crypto/sha512_generic.c arch/arm/crypto/aes-armv4.S  arch/a
 ####v3.18
 git checkout HEAD -- lib/lzo/lzo1x_decompress_safe.c
 
-# cleanup
-find * | grep "\.h" | awk '{print "chmod 664 " $1}' | sh -
-find * | grep "\.c" | awk '{print "chmod 664 " $1}' | sh -
-find * | grep "Makefile" | awk '{print "chmod 664 " $1}' | sh -
-find * | grep "Kconfig" | awk '{print "chmod 664 " $1}' | sh -
-
 # for recheck: lib/genalloc.c include/linux/genalloc.h
+# use code from 78be3176c4335b8ff3d9625ed3fc571e7d2ba8f4 for reboot chip saga_reset/spade_reset
+# rewrite arch/arm/mach-msm/clock-7x30.c:msm_clocks_7x30 to official way
